1a55d02053c93c8cfca08d6cbe1e02cf4528cbf2
Cristhian <cristhian19990302@gmail.com>
2025-06-15 19:24:46 -0500

    primer commit

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  base: '/portafolio/',
+  site: 'https://cristhiandevcod.github.io/portafolio/',
+  vite: {
+      plugins: [tailwindcss()],
+      resolve: {
+        alias: {
+          "@": "/src",
+        }
+      }
+  },
+
+  integrations: [react()]
+});
---
+++ src/lib/data.ts
@@ -0,0 +1,164 @@
+export const personalInfo = {
+  name: "Cristhian Velasquez",
+  location: "Medellín, Colombia",
+  email: "cristhian19990302@gmail.com",
+  github: "https://github.com/CristhianDevCod",
+  linkedin: "https://www.linkedin.com/in/cristhian1999/",
+};
+
+export const workExperience = [
+  {
+    company: "AlmaContact",
+    location: "Presencial",
+    position: "Practicante Full Stack",
+    period: "Febrero 2025 - Actualmente",
+    achievements: [
+      "Diseñe, desarrollé e implementé una plataforma completa para la gestión de KPIs (Indicadores Clave de Desempeño), optimizando los procesos operativos y mejorando la eficiencia organizacional en toda la empresa",
+      "Implementé una arquitectura robusta y escalable para la plaforma siempre pensando en la modularidad y reusabilidad, integrando tecnologías modernas para garantizar rendimiento y seguridad",
+      "Automatice la generación de metas, para facilitar la gestion por parte del usuario",
+      "Integré un modelo de inteligencia artificial (LLM) para realizar analisis de causa raíz de salidas no conformes, sugiriendo planes de acción específicos, lo que redujo tiempos de analisis.",
+      "Colaboré con equipos multifuncionales para garantizar que la solución cumpliera con los objetivos estratégicos de la empresa, obteniendo retroalimentación constante y ajustando funcionalidades según las necesidades.",
+      "Documenté y realicé pruebas del sistema, asegurando un despliegue exitoso y un mantenimiento eficiente a largo plazo.",
+      "Logré llevar la herramienta al siguiente nivel, transformándola en un activo estratégico clave para la organización."
+    ],
+  },
+  // {
+  //   company: "Iberoamericana",
+  //   location: "Bogotá, Colombia",
+  //   position: "Estudiante",
+  //   period: "Mar 2023 - Mar 2024",
+  //   achievements: [
+  //     "Developed responsive frontend components using React.js, enhancing user experience and app performance.",
+  //     "Optimized JavaScript solutions to improve load times and data-fetching efficiency.",
+  //   ],
+  // }
+];
+
+export const education = [
+  {
+    institution: "Universidad Iberoamericana",
+    location: "Bogotá, Colombia",
+    degree: "Ingeniero de software",
+    period: "Febrero 2021 - En curso",
+    achievements: [
+      "Adquirí conocimientos avanzados en el uso de patrones de diseño para desarrollar soluciones de software eficientes, reutilizables y escalables.",
+      "Comprendí y apliqué los diferentes modelos del ciclo de vida, desde la planificación hasta el mantenimiento, asegundao la entrega de productos de calidad.",
+      "Aprendí a coordinar y dirigir proyectos utilizando metogologías ágiles y tradicionales, priorizando la organización, la comunicación y el cumplimiento de plazos.",
+      "Desarrollé un enfoque crítifico para aplicar principios ingenieriles en el diseño y desarrollo de software, garantizando que los sistemas resuelvan problemas reales de manera efectiva.",
+      "Interioricé la importancia de no solo cumplir con los requisitos técnicos del cliente, si no de entender y satisfacer sus necesidades reales, aportando valor adicional a los proyectos.",
+      "Participé activamente en proyectos y actividades académicas que fomentaron la innovación, el trabajo en equipo y la resolución de problemas complejos.",
+      "Comprendí la relevancia de la ética en la toma de decisiones técnicas y en la interacción con equipos de trabajo y stakeholders."
+    ],
+  },
+];
+export const skills = {
+  programmingLanguages: [
+    "JavaScript",
+    "TypeScript",
+    "Python",
+    "Java",
+    "C#",
+  ],
+  frontendDevelopment: [
+    "HTML",
+    "CSS",
+    "Reactjs",
+    "Bootstrap",
+    "React Native",
+    "Tailwind CSS",
+  ],
+  backendDevelopment: ["Nodejs"],
+  databaseAndStorage: ["MySQL", "NoSQL"],
+  // cloudAndDevOps: ["AWS"],
+  toolsAndServices: [
+    "Clerk (Auth)",
+    "Sanity (CMS)",
+    "Tinybird (analytics)",
+    "Zod",
+    "Sentry",
+    "Mixpanel",
+    "Trigger.dev",
+  ],
+};
+
+export const projects  = [
+  {
+    title: "Gestor de KPIs",
+    github: "https://www.canva.com/design/DAGl6zbqYdQ/km96CO0dxSPaZOUjQf5FGQ/edit?utm_content=DAGl6zbqYdQ&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
+    description: [
+      "Diseñé y desarrollé un sistema integral para la gestión de KPIs (Indicadores Clave de Desempeño), orientado a mejorar la eficiencia y el seguimiento de indicadores estratégicos en la organización.",
+      "Implementé funcionalidades para crear, editar, visualizar detalles y deshabilitar indicadores, permitiendo un control completo y centralizado.",
+      "Desarrollé una sección de historial que registra las acciones realizadas sobre cada KPI, incluyendo información sobre quién lo creó, modificó o deshabilitó, asegurando trazabilidad y transparencia.",
+      "Implementé módulos para asignar indicadores a áreas específicas, servicios o directamente a personal operativo o staff, promoviendo la responsabilidad y el seguimiento individualizado.",
+      "Creé una funcionalidad para definir metas operativas, administrativas e individuales para cada KPI, asegurando objetivos claros y medibles.",
+      "Incluí una sección para registrar y actualizar los resultados asociados a las metas establecidas, facilitando el análisis de desempeño.",
+      "Desarrollé un módulo específico para tratar indicadores que no cumplen con sus metas, gestionando planes de acción completos que incluyen análisis de causa raíz, asignación de responsables y seguimiento de actividades correctivas.",
+      "Apliqué principios de diseño modular y buenas prácticas de desarrollo para garantizar un sistema escalable, seguro y eficiente.",
+    ],
+  },
+  {
+    title: "Alerta ciudadana",
+    github: "https://alerta-ciudadana-6da98.web.app/",
+    description: [
+      "Diseñé y desarrollé una página web interactiva para la publicación y visualización de noticias, promoviendo la participación ciudadana en tiempo real.",
+      "Implementé un sistema de autenticación seguro, permitiendo a los usuarios registrarse e iniciar sesión mediante correo electrónico y contraseña o a través del servicio de autenticación de Google.",
+      "Creé un flujo intuitivo para la creación y gestión de noticias, incluyendo campos como título, descripción e imagen, con una interfaz amigable y responsive.",
+      "Configuré la integración con Firebase para gestionar la base de datos en tiempo real, almacenar imágenes en Firebase Storage y sincronizar la información de manera eficiente.",
+      "Desarrollé un menú de inicio que permite a los usuarios visualizar las noticias publicadas por otros, incluyendo una funcionalidad de detalle para obtener información completa sobre cada publicación.",
+      "Apliqué buenas prácticas de desarrollo, modularizando componentes y optimizando el rendimiento de la aplicación para garantizar una experiencia fluida.",
+      "Documenté y probé el proyecto para asegurar su funcionalidad y usabilidad, logrando una solución estable y escalable."
+    ],
+  },
+];
+
+export const awards = [
+  {
+    name: "Ingenieria de software",
+    issuer: "Universidad Iberoamericana",
+    date: "Actualmente",
+    type: "International",
+    position: "Ingeniero de software",
+  },
+  {
+    name: "Desarrollo web Full Stack",
+    issuer: "UdeCatanluña",
+    date: "Noviembre de 2023",
+    type: "National",
+    position: "Certificado",
+  },
+  {
+    name: "Java Developer",
+    issuer: "Ucoltis",
+    date: "Diciembre de 2023",
+    type: "National",
+    position: "Certificado",
+  },
+  {
+    name: "Angular",
+    issuer: "Udemy",
+    date: "Enero de 2025",
+    type: "National",
+    position: "Certificador",
+  },
+  {
+    name: "JavaScript",
+    issuer: "Udemy",
+    date: "Julio de 2024",
+    type: "National",
+    position: "Certificado",
+  },
+  {
+    name: "Angular",
+    issuer: "Sololearn",
+    date: "Agosto de 2024",
+    type: "National",
+    position: "Certificado",
+  },
+  {
+    name: "Framework Angular",
+    issuer: "Iberoamericana",
+    date: "Abril de 2024",
+    type: "National",
+    position: "Certificado",
+  },
+];
